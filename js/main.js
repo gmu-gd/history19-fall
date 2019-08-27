b9ad6ade8a801bcbc6e21c984b9fbf4c0568260a
Michael M <mmcdermott18@users.noreply.github.com>
2019-08-26 22:34:58 -0400

    add some js back

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     // run function on initial page load
+    studentHeight();
     menuSlide();
+
+    // printLink();
 
     // run function on resize of the window
     $(window).resize(function() {
-
+      studentHeight();
     });
     // run function on scroll
     $(window).scroll(function() {
@@ -12,6 +15,17 @@ $(document).ready(function() {
     });
 });
 
+var textHeight = $('.student h2').outerHeight();
+
+function studentHeight() {
+  var headHeight = $('header').outerHeight();
+  var pageHeight = $(window).height();
+  var height = ((pageHeight - headHeight) / 4);
+  var topPadding = ((height - textHeight) / 2);
+  $('.student').css("height", height);
+  $('.menu-list').css("height", (height * 4));
+  $('.student h2').css("padding-top", topPadding);
+}
 
 function menuSlide() {
   $('.menu-icon').click(function() {
